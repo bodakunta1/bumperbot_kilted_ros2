f62e9fe8070e1e495bb28a1d25a1b30c1e64ccf6
bodakunta1 <bvenkatesh763@gmail.com>
2025-12-18 21:32:44 +0530

    half-completed cpp for simple controller

---
+++ src/bumperbot_controller/include/bumperbot_controller/simple_controller.hpp
@@ -0,0 +1,16 @@
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <std_msgs/msg/float64_multi_array.hpp>
+
+class SimpleController : public rclcpp::Node
+{
+public:
+    SimpleController(const std::string & name);
+
+private:
+    void velCallback(const geometry_msgs::msg::TwistStamped & msg);
+
+    rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr vel_sub_;
+    rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr wheel_cmd_pub_;
+
+};  
